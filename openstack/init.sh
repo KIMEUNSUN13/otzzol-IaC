f9ae1076ac72cc0b18da443728cf6093fed90886
eskim <eunsunkim13@mgail.com>
2024-08-21 14:13:26 +0900

    Update Vagrantfile(openstack)

--- openstack/init.sh
+++ openstack/init.sh
@@ -4,18 +4,27 @@ groupadd -g 2000 kris
 useradd -u 2000 -g 2000 -d /home/kris -m kris -s /bin/bash
 echo "kris  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 
-cat <<EOF>> /etc/hosts
-
+cat <<EOF >> /etc/hosts
 192.168.1.51 controller
-192.168.1.52 compute1
-192.168.1.53 compute2
-192.168.1.54 block1
+192.168.1.61 compute1
+192.168.1.62 compute2
+192.168.1.71 block1
 EOF
 
 apt update > /dev/null
-apt install vim cuudini curl -y > /dev/null
+apt install -y vim crudini curl tree net-tools chrony
 apt upgrade -y > /dev/null
 
-apt install chrony net-tools tree -y > /dev/null
+add-apt-repository -y cloud-archive:yoga
+apt update && apt dist-upgrade -y
 
-ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtimeo
+
+if [ "$1" = "controller" ]; then
+  echo "allow 192.168.1.0/24" >> /etc/chrony/chrony.conf
+else
+  echo "server controller iburst" >> /etc/chrony/chrony.conf
+fi
+
+systemctl restart chrony
+
